7ef51c8f07f507ee465a662a5e2aa1be8698f2b5
Judit Vives <118683186+jvivesramon@users.noreply.github.com>
2023-05-13 16:30:00 +0200

    Revert "Add the create express and listen for connections"

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-import "./loadEnviroment.js";
-import createDebug from "debug";
-import chalk from "chalk";
-import { app } from "./server/index.js";
 
-const debug = createDebug("robots-api:root");
-
-const port = process.env.PORT ?? 4000;
-
-app.listen(port, () => {
-  debug(chalk.blue(`Listening on http://localhost:${port}`));
-});
--- src/server/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import morgan from "morgan";
-
-export const app = express();
-
-app.use(morgan("dev"));
-
-app.use(express.json());
