0c880afeab7551b79e534c15f5cd82d9eba50500
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-05-14 20:52:58 +0200

    Add cors permissions

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 import { robotsRouter } from "./routers/robotsRouter.js";
 import { generalError, notFoundError } from "./middleware/errorControllers.js";
 
 export const app = express();
+
+const corsOptions = {
+  origin:
+    "mongodb+srv://jvivesramon:fantasia@fantasia.w5j49w4.mongodb.net/robotsIJ",
+  optionsSuccessStatus: 200,
+  methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+};
+
+app.use(cors(corsOptions));
 
 app.disable("x-powered-by");
 
