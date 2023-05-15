a7c39bfe2b5e4ddca31dda38edc5d4a0ba00700e
Judit Vives <jvivesramon@gmail.com>
2023-05-16 00:18:30 +0200

    Add user schema

---
+++ src/database/schema/User.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    min: 5,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = model("User", userSchema, "users");
